20f146c1514f2592230d83544052befd49affda6
Zakjer <340631@uwr.edu.pl>
2026-01-06 14:26:32 +0100

    test

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-flgkpkk$wtu)3s8z@dtqsh)pek65hpoh3f5*s79blzdz!33h5h'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
