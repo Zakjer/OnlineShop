a95487c9e340dd6f2ef21fa292cb4bc947bc6424
Zakjer <340631@uwr.edu.pl>
2026-01-22 20:44:25 +0100

    Adding ngrok

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,10 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
 
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
